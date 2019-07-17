d0c8cf33fecdabfb8fb11e1312676795fe0b8b35
Tiina Malinen <malitsu@gmail.com>
2019-07-17 18:07:14 +0300

    Exercise 3.1

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "Alli Kuikamoinen",
+      "number": "123 123",
+      "date": "2019-07-13T13:57:26.799Z",
+      "important": true,
+      "id": 5
+    },
+    {
+      "name": "Timo Hokkanen",
+      "number": "000 0000",
+      "date": "2019-07-14T15:42:29.547Z",
+      "important": true,
+      "id": 6
+    }
+  ]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
