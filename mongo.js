eb9dbdb14c24f99ad2d91733ea512ba03b4fa19e
Tiina Malinen <malitsu@gmail.com>
2019-07-31 20:01:24 +0300

    Exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@klusteri-omwho.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if ( process.argv.length == 3 ) {
+  Person
+  .find({})
+  .then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(person.name +" " +person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+else {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+    date: new Date()
+  })
+  
+  person.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  })
+}
